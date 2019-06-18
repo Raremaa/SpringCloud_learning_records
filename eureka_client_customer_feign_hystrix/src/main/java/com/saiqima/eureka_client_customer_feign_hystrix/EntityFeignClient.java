0305d205f73a58d0e03c039b3a3c406bd5a15100
Raremaa <masaiqi1996@icloud.com>
2019-06-18 16:47:20 +0800

    完成hystrix与ferign的整合

--- eureka_client_customer_feign_hystrix/src/main/java/com/saiqima/eureka_client_customer_feign_hystrix/EntityFeignClient.java
+++ eureka_client_customer_feign_hystrix/src/main/java/com/saiqima/eureka_client_customer_feign_hystrix/EntityFeignClient.java
@@ -1,5 +1,8 @@
 package com.saiqima.eureka_client_customer_feign_hystrix;
 
+import feign.hystrix.FallbackFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,13 +18,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 /**
  * 注意注解@FeignClient 由于使用了Eureka,Ribbon会把注解值转化为Eureka服务注册表中的服务
  * 还可使用url指定请求url,例如@FeignClient(name=xxx,url=xxx)
+ * fallback = FeignClientFallback.class 不需要查看回退信息
+ * fallbackFactory = FeignClientFallbackFactory.class 需要查看回退信息
  */
-@FeignClient(name = "eureka-client-provider", fallback = FeignClientFallback.class)
+@FeignClient(name = "eureka-client-provider", fallbackFactory = FeignClientFallbackFactory.class)
 public interface EntityFeignClient {
 
 	@GetMapping("/provider/go/{requestStr}")
 	Entity go(@PathVariable String requestStr);
 }
+
 
 @Component
 class FeignClientFallback implements EntityFeignClient{
@@ -31,3 +37,24 @@ class FeignClientFallback implements EntityFeignClient{
 		return entity;
 	}
 }
+
+/**
+ * 当需要查看回退原因,就需要通过Fallback Factory进行查看
+ * 方式是回退类实现FallbackFactory类
+ */
+@Component
+class FeignClientFallbackFactory implements FallbackFactory<EntityFeignClient> {
+	private static final Logger LOG = LoggerFactory.getLogger(FeignClientFallbackFactory.class);
+	@Override
+	public EntityFeignClient create(Throwable throwable) {
+		return new EntityFeignClient() {
+			@Override
+			//日志最好放在各个fallback方法中
+			public Entity go(String requestStr) {
+				LOG.info(throwable.toString());
+				return null;
+			}
+		};
+	}
+}
+
